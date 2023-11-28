cfe61938967bfa4af7291e70630c10faf5ed33a7
lokesh21012002 <83236804+lokesh21012002@users.noreply.github.com>
2023-11-28 14:07:24 +0530

    Update WebSecurityConfig.java

--- src/main/java/com/example/travelplan/config/WebSecurityConfig.java
+++ src/main/java/com/example/travelplan/config/WebSecurityConfig.java
@@ -80,7 +80,7 @@ public class WebSecurityConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://main.d37n05dh3jw68a.amplifyapp.com"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
