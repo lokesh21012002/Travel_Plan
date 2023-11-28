78bda7ce35bd5758a8be4f6fee73c3f6a6ec729e
lokesh21012002 <83236804+lokesh21012002@users.noreply.github.com>
2023-11-28 20:15:22 +0530

    Update WebSecurityConfig.java

--- src/main/java/com/example/travelplan/config/WebSecurityConfig.java
+++ src/main/java/com/example/travelplan/config/WebSecurityConfig.java
@@ -80,7 +80,7 @@ public class WebSecurityConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://travel-plan-client.s3-website.ap-south-1.amazonaws.com"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://travel-plan-client.s3-website.ap-south-1.amazonaws.com/**"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
